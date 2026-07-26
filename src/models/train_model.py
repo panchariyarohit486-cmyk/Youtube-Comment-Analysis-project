97195133b519264efdbaaa79ce567274f3b1fc22
Rohit Panchariya <panchariyarohit486@gmail.com>
2026-07-26 23:58:00 +0530

    same model becouse thundersvm not working properly

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -6,9 +6,6 @@ import yaml
 import numpy as np
 import scipy.sparse as sp
 
-from sklearnex import patch_sklearn
-patch_sklearn()  # must run BEFORE importing SVC — patches sklearn to use Intel oneDAL (multi-core)
-
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, classification_report
 
